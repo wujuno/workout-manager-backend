4a08a525ba5fb4413d8708be9ee14d604eda5487
wujuno <86146780+wujuno@users.noreply.github.com>
2022-12-21 12:49:10 +0900

    Setup Server

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 import { ApolloServer, gql } from "apollo-server";
 
-const server = new ApolloServer({})
+const typeDefs = gql`
+    type Query {
+        hello : String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => "world"
+    },
+};
+
+const server = new ApolloServer({typeDefs, resolvers})
 
 server.listen().then(({url})=> {
     console.log(`Running on ${url}`);

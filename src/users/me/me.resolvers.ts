73e21fcc7681f45e7e1ab966de0d50c3a4cc198a
wujuno <86146780+wujuno@users.noreply.github.com>
2023-01-03 18:43:53 +0900

    Add me

---
+++ src/users/me/me.resolvers.ts
@@ -0,0 +1,14 @@
+import { Resolvers } from "../../type";
+import { protectedResolver } from "../users.utils";
+
+const resolvers:Resolvers = {
+    Query:{
+        me: protectedResolver(
+            (_,__,{client,loggedInUser})=> client.user.findUnique({
+                where:{id:loggedInUser.id}
+            })
+        )
+    }
+}
+
+export default resolvers;

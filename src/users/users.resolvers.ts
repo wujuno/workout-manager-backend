6a46f47387e6f83637d50d994a638b46160dec38
wujuno <huj88854@gmail.com>
2023-03-13 16:01:56 +0900

    Feat: 기록 정렬

--- src/users/users.resolvers.ts
+++ src/users/users.resolvers.ts
@@ -1,28 +1,26 @@
 import { Resolvers } from "../type";
 import { protectedResolver } from "./users.utils";
 
-
-const resolvers:Resolvers = {
-    User:{
-        isMe: ({id},_,{loggedInUser,client}) => {
-            if(!loggedInUser){
-                return false
-            }
-            return id === loggedInUser.id
-        },
-        records: protectedResolver(
-            async({id},__,{loggedInUser,client})=>{
-                if(!loggedInUser){
-                    return false
-                }
-                return await client.record.findMany({
-                    where:{userId:id},
-                    select:{id:true,date:true,items:true},
-                    take:3
-                })
-            }
-        )
-    }
-}
+const resolvers: Resolvers = {
+  User: {
+    isMe: ({ id }, _, { loggedInUser, client }) => {
+      if (!loggedInUser) {
+        return false;
+      }
+      return id === loggedInUser.id;
+    },
+    records: protectedResolver(async ({ id }, __, { loggedInUser, client }) => {
+      if (!loggedInUser) {
+        return false;
+      }
+      return await client.record.findMany({
+        orderBy: { createdAt: "desc" },
+        where: { userId: id },
+        select: { id: true, date: true, items: true },
+        take: 3,
+      });
+    }),
+  },
+};
 
 export default resolvers;
